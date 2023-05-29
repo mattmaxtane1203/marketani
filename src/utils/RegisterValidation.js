1bd3c8abdbb1a47092c3d0803c5d1b67cb0c84bf
mattmaxtane1203 <mattmaxtane1203@gmail.com>
2023-05-29 10:10:00 +0700

    Edited Register Validation functions

--- src/utils/RegisterValidation.js
+++ src/utils/RegisterValidation.js
@@ -1,32 +1,41 @@
 export const RegisterValidation = {
-    nomorTeleponIsValid: (nomorTelepon) => {
-        const regex = new RegExp(/^(\+62|62|0)[2-9]{1}[0-9]{7,11}$/)
-    
-        // TODO: Check if phone number exists in database
-    
-        if(nomorTelepon.test(regex) && nomorTelepon.length <= 12){
-            return "Nomor telepon tidak sesuai";
-        }
+  nomorTeleponIsValid: (nomorTelepon) => {
+    const regex = new RegExp(/^(\+62|62|0)[2-9]{1}[0-9]{7,11}$/);
 
-        return null;
-    },
-    
-    nameIsValid: (namaLengkap) => {
-    
-        const regex = new RegExp(/^([A-Z][a-z]*\s)+[A-Z][a-z]*$/)
-    
-        return namaLengkap.test(regex)
-    },
+    // TODO: Check if phone number exists in database
 
-    passwordIsValid: (password) => {
-
-        const regex = new RegExp(/^(?=.*[0-9a-zA-Z])(?=.*[!@#$%^&*])(?=.{8,})/)
-
-        return password.test(regex)
-    },
-    
-    passwordMatches: (password, passwordConfirmation) => {
-        
-        return password.localeCompare(passwordConfirmation) === 0;
+    if (nomorTelepon.test(regex) && nomorTelepon.length <= 12) {
+      return "Nomor telepon tidak sesuai";
     }
-}
+
+    return null;
+  },
+
+  nameIsValid: (namaLengkap) => {
+    const regex = new RegExp(/^([A-Z][a-z]*\s)+[A-Z][a-z]*$/);
+
+    if (namaLengkap.test(regex) == false) {
+      return "Nama harus mulai dengan huruf besar dan lengkap";
+    }
+
+    return null;
+  },
+
+  passwordIsValid: (password) => {
+    const regex = new RegExp(/^(?=.*[0-9a-zA-Z])(?=.*[!@#$%^&*])(?=.{8,})/);
+
+    if (password.test(regex) == false) {
+      return "Kata sandi harus menggunakan setidaknya satu huruf besar dan satu huruf kecil, satu karakter spesial dan 8 karakter atau lebih";
+    }
+
+    return null;
+  },
+
+  passwordMatches: (password, passwordConfirmation) => {
+    if (password.localeCompare(passwordConfirmation) === 0) {
+      return "Kata sandi tidak sama";
+    }
+
+    return null;
+  },
+};

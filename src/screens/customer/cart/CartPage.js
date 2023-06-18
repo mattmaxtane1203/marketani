5512e193f3056267fc6b77d50d1ad8d49ca588ed
mattmaxtane1203 <mattmaxtane1203@gmail.com>
2023-06-18 22:49:20 +0700

    (Incomplete) Cart functionality

--- src/screens/customer/cart/CartPage.js
+++ src/screens/customer/cart/CartPage.js
@@ -1,11 +1,11 @@
 import {
-    ScrollView,
-    Text,
-    StyleSheet,
-    View,
-    Image,
-    SafeAreaView,
-    TouchableOpacity
+  ScrollView,
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
 } from "react-native";
 import Icons from "../../../constants/Icons";
 import Images from "../../../constants/Images";
@@ -14,18 +14,66 @@ import CartSearchBar from "../../../components/input/CartSearchBar";
 import ListToko from "./ShopList";
 import ShopList from "./ShopList";
 import ShopName from "./ShopName";
+import { CartContext } from "../../../contexts/CartContext";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+// Steps
+// Nge-fetch items yang ada di Cart Context
+// Map semua items di custom component "ShopList"
+// Di component ShopList, bikin function di mana kalo pencet plus dan minus button, quantity dari item tersebut di Cart bisa ganti (tambah atau kurangin)
+// Bikin button Checkout untuk bikin Transaction Header dan Details
+    // Ingat kalo setiap Transaction Header itu per Seller
+    // Kalo misalkan ada dua item dari beda seller, harus dua transaction yang berbeda
+// Kalo checkout
+    // Post semua item yang ada di cart ke transaction masing-masing
+    // Display transactionnya di daftar transaction di customer dan seller
+    // Cartnya di-empty
+
 
 const CartPage = ({ navigation }) => {
-    return (
-        <View style={style.background}>
-            <SafeAreaView>
-                <View style={style.header}>
-                    <BackButton onPress={() => navigation.navigate()} />
-                    <CartSearchBar placeholder={"Cari Produk"} />
-                </View>
+  const [product, setProduct] = useState([]);
+  const { cartItems } = useContext(CartContext);
 
-                <ScrollView>
-                    <View style={style.container}>
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(
+        `http://${currentIP}:8081/product/getProduct/${item.productId}`
+      );
+      setProduct(response.data.product);
+    } catch (error) {
+      console.error("Error occurred while fetching product:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  return (
+    <View style={style.background}>
+      <SafeAreaView>
+        <View style={style.header}>
+          <BackButton onPress={() => navigation.navigate()} />
+          <CartSearchBar placeholder={"Cari Produk"} />
+        </View>
+
+        <ScrollView>
+          {cartItems.map((item) => (
+            <View style={style.container} key={item.productId}>
+              <View>
+                <ShopName ShopText={"Seller ID: " + item.sellerId} />
+                <ShopList
+                  productImage={Images.tomatHijau}
+                  title={item.productId}
+                  price={item.productId}
+                  quantity={item.quantity}
+                />
+              </View>
+            </View>
+          ))}
+
+          {/* <View style={style.container}>
                         <View>
                             <ShopName ShopText="Fresh Shop" />
                             <ShopList
@@ -34,54 +82,37 @@ const CartPage = ({ navigation }) => {
                                 price={"Rp15.000"}
                             />
                         </View>
-                    </View>
-                    <View style={style.container}>
-                        <View>
-                            <ShopName ShopText="Lokal Shop" />
-                            <ShopList
-                                productImage={Images.tomatManis}
-                                title={"Tomat Merah (500gr)"}
-                                price={"Rp15.000"}
-                            />
-                            <ShopList
-                                productImage={Images.tomatManis}
-                                title={"Tomat Merah (300gr)"}
-                                price={"Rp15.000"}
-                            />
-                        </View>
-
-                    </View>
-                </ScrollView>
-
-            </SafeAreaView>
-        </View>
-    )
-}
+                    </View> */}
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
+};
 
 const style = StyleSheet.create({
-    background: {
-        backgroundColor: '#FFFFFF',
-        flex: 1
-    },
-    headerBackground: {
-        backgroundColor: '#F1F1F1',
-        flex: 1
-    },
-    header: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        paddingHorizontal: 20,
-        paddingVertical: 15,
-        borderBottomColor: "#CFCFCF",
-        borderBottomWidth: 0.5,
-    },
-    container: {
-        paddingTop: 10,
-        borderColor: "#F1F1F1",
-        borderTopWidth: 10
-    }
-})
+  background: {
+    backgroundColor: "#FFFFFF",
+    flex: 1,
+  },
+  headerBackground: {
+    backgroundColor: "#F1F1F1",
+    flex: 1,
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderBottomColor: "#CFCFCF",
+    borderBottomWidth: 0.5,
+  },
+  container: {
+    paddingTop: 10,
+    borderColor: "#F1F1F1",
+    borderTopWidth: 10,
+  },
+});
 
 export default CartPage;
--- src/screens/customer/cart/ShopList.js
+++ src/screens/customer/cart/ShopList.js
@@ -1,64 +1,96 @@
-import { TouchableOpacity, Image, StyleSheet, View, Text, SafeAreaView, FlatList } from "react-native";
-import React, { useState } from 'react'
-import { AntDesign } from '@expo/vector-icons';
+import {
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import React, { useContext, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
 import ShopName from "./ShopName";
-import Checkbox from 'expo-checkbox'
+import Checkbox from "expo-checkbox";
 import Images from "../../../constants/Images";
-import { style } from "deprecated-react-native-prop-types/DeprecatedTextPropTypes";
-import ProductQuantity from "../../../components/input/productQuantity";
+import { CartContext } from "../../../contexts/CartContext";
 
-const ShopList = ({ onPress, productImage, title, price }) => {
-    const [isChecked, setChecked] = useState(false);
-    return (
-        <TouchableOpacity onPress={onPress}>
-            <View style={styles.ShopListBox}>
-                <Checkbox
-                    style={styles.checkbox}
-                    value={isChecked}
-                    onValueChange={setChecked}
-                    color={isChecked ? '#48BD5B' : undefined} />
-                <Image source={productImage} style={styles.Image} />
-                <View style={styles.productInfo}>
-                    <Text numberOfLines={1} style={styles.productName}>{title}</Text>
-                    <Text style={styles.productPrice}>{price}</Text>
-                    <ProductQuantity />
-                </View>
-            </View>
-        </TouchableOpacity>
-    );
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+
+const ShopList = ({ onPress, productImage, title, price, quantity }) => {
+  const [isChecked, setChecked] = useState(false);
+  const { cartItems, addToCart, removeFromCart } = useContext(CartContext);
+
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.ShopListBox}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+          color={isChecked ? "#48BD5B" : undefined}
+        />
+        <Image source={productImage} style={styles.Image} />
+        <View style={styles.productInfo}>
+          <Text numberOfLines={1} style={styles.productName}>
+            {title}
+          </Text>
+          <Text style={styles.productPrice}>{price}</Text>
+          <View style={styles.row}>
+            <TouchableOpacity>
+              <AntDesign name="minuscircleo" size={24} color="#48BD5B" />
+            </TouchableOpacity>
+            <Text marginHorizontal={20}>{quantity}</Text>
+            <TouchableOpacity>
+              <AntDesign name="pluscircleo" size={24} color="#48BD5B" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    ShopListBox: {
-        paddingHorizontal: 20,
-        display: "flex",
-        flexDirection: "row",
-        paddingBottom: 25
-    },
-    checkbox: {
-        marginLeft: 20
-    },
-    Image: {
-        width: 80,
-        height: 80,
-        marginLeft: 20,
-        marginRight: 10,
-        borderRadius: 15,
-    },
-    productInfo: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        marginStart: 12
-    },
-    productPrice: {
-        fontSize: 15,
-        marginTop: 4
-    },
-    productName: {
-        fontSize: 16,
-        fontWeight: "bold"
-    }
-})
+  ShopListBox: {
+    paddingHorizontal: 20,
+    display: "flex",
+    flexDirection: "row",
+    paddingBottom: 25,
+  },
+  checkbox: {
+    marginLeft: 20,
+  },
+  Image: {
+    width: 80,
+    height: 80,
+    marginLeft: 20,
+    marginRight: 10,
+    borderRadius: 15,
+  },
+  productInfo: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    marginStart: 12,
+  },
+  productPrice: {
+    fontSize: 15,
+    marginTop: 4,
+  },
+  productName: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+
+    marginVertical: screenHeight * 0.015,
+  },
+});
 
 export default ShopList;

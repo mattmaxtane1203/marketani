5665ac4ae00574bb24697a133a425c17c5cbf979
Glennix <92360411+Glennix27@users.noreply.github.com>
2023-06-27 23:18:35 +0700

    Fixed Fetch Transaction ID

--- backend/controllers/TransactionController.js
+++ backend/controllers/TransactionController.js
@@ -1,35 +1,34 @@
-const TransactionRepository = require("../repositories/TransactionRepository")
-
+const TransactionRepository = require("../repositories/TransactionRepository");
 const db = require("../config/database");
 
 const createTransaction = async (customerId, separatedItems) => {
+  try {
+    for (const sellerId in separatedItems) {
+      const transactionHeader = {
+        customerId,
+        sellerId,
+        transactionDate: new Date(),
+        transactionTime: new Date(),
+        statusPemesanan: "Sedang Diproses",
+      };
 
-    try {
-        for (const sellerId in separatedItems) {
-            const transactionHeader = {
-                customerId,
-                sellerId,
-                transactionDate: new Date(),
-                transactionTime: new Date(),
-                statusPemesanan: "Sedang Diproses",
-            };
+      const { insertId } = await TransactionRepository.insertTransactionHeader(transactionHeader);
+      console.log("Insert ID: " + insertId);
+      const items = separatedItems[sellerId];
+      const transactionDetails = items.map((item) => ({
+        transactionId: insertId, // Use insertId here
+        productId: item.productId,
+        quantity: item.quantity,
+      }));
 
-            const { insertId } = await TransactionRepository.insertTransactionHeader(transactionHeader);
-            console.log("Insert ID: " + insertId)
-            const items = separatedItems[sellerId];
-            const transactionDetails = items.map((item) => ({
-                transactionId: insertId,
-                productId: item.productId,
-                quantity: item.quantity,
-            }));
-
-            await TransactionRepository.insertTransactionDetails(transactionDetails);
-        }
-    } catch (error) {
-        throw new Error("Error creating transactions");
+      await TransactionRepository.insertTransactionDetails(transactionDetails);
     }
+  } catch (error) {
+    console.log("ERROR at TransactionController " +error)
+    throw new Error("Error creating transactions");
+  }
 };
 
 module.exports = {
-    createTransaction,
+  createTransaction,
 };
--- backend/repositories/TransactionRepository.js
+++ backend/repositories/TransactionRepository.js
@@ -4,22 +4,24 @@ const db = require("../config/database");
 // Function to insert a transaction header
 const insertTransactionHeader = async (transactionHeader) => {
     const { customerId, sellerId, transactionDate, transactionTime, statusPemesanan } = transactionHeader;
-
+    
     const query = "INSERT INTO TransactionHeader (customer_id, seller_id, transaction_date, transaction_time, status_pemesanan) VALUES (?, ?, ?, ?, ?)";
     const values = [customerId, sellerId, transactionDate, transactionTime, statusPemesanan];
 
-    const result = await db.query(query, values);
-    // db.query(query, values, function (err, result) {
-    //     if (err) {
-    //         console.error("Error executing the query: " + err.stack);
-    //         return res.status(500).json({ error: "Failed to register the user." });
-    //     }
-    //     console.log("User registered with Customer ID: " + result.insertId);
-    //     return res.status(200).json({ insertId: result.insertId });
-    //     });
-    console.log(result)
-    return { insertId: result.insertId };
+    return new Promise((resolve, reject) => {
+        db.query(query, values, function (err, result) {
+            if (err) {
+            console.error("Error executing the query: " + err.stack);
+            reject(new Error("Failed to insert transaction header."));
+            } else {
+            console.log("User registered with Seller ID: " + result.insertId);
+            resolve({ insertId: result.insertId });
+            }
+        });
+    });
+    
 };
+
 
 // Function to insert transaction details
 const insertTransactionDetails = async (transactionDetails) => {
